4acf23f5be8e1bdaed2ad96daee82692ccd96563
David Ortega <david@dobdev.com>
2013-08-28 16:13:07 +0200

    Added frontend for email log

--- classes/controller/admin/emaillog.php
+++ classes/controller/admin/emaillog.php
@@ -11,24 +11,27 @@ class Controller_Admin_Emaillog extends Admincontroller {
 
 	public function action_index()
 	{
+		$page = 1;
+
 		if (isset($_GET['page']))
 		{
-			$emailqueue  = Emailqueue::factory();
-			$count       = $emailqueue->get_count_emails();
-
-			$page                         = intval($_GET['page']);
-			if ($page<1) $page            = 1;
-
-			$limit                        = 10;
-			$offset                       = ($page-1)*$limit;
-			if ($offset > $count) $offset = 0;
-			$pages                        = ceil($count/$limit);
-			$actual_page                  = floor($offset/$limit)+1;
-
-			$emails      = $emailqueue->get_emails(NULL, $limit, $offset);
-
-			xml::to_XML($emails, array('emails' => $this->xml_content), 'email', 'id');
-			xml::to_XML(array('email_count' => $count, 'pages'=>$pages, 'actual_page'=>$actual_page), array('mails_meta'=>$this->xml_content));
+			$page = intval($_GET['page']);
 		}
+
+		$emailqueue                   = Emailqueue::factory();
+		$count                        = $emailqueue->get_count_emails();
+	
+		if ($page<1) $page            = 1;
+
+		$limit                        = 10;
+		$offset                       = ($page-1)*$limit;
+		if ($offset > $count) $offset = 0;
+		$pages                        = ceil($count/$limit);
+		$actual_page                  = floor($offset/$limit)+1;
+
+		$emails      = $emailqueue->get_emails(NULL, $limit, $offset);
+
+		xml::to_XML($emails, array('emails' => $this->xml_content), 'email', 'id');
+		xml::to_XML(array('email_count' => $count, 'pages'=>$pages, 'actual_page'=>$actual_page, 'limit'=>$limit), array('mails_meta'=>$this->xml_content));
 	}
 }
